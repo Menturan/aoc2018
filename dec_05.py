cb22319774503359526975e009f875fcbf570b20
Menturan <ohlander91@gmail.com>
2018-12-05 22:23:04 +0100

    Dec 5 - Complete!

--- dec_05.py
+++ dec_05.py
@@ -1,19 +1,19 @@
 from puzzle_util.christmas_tree import tree
 from puzzle_util.read_file import read_file_to_string
 from puzzle_util.test import test
+from timeit import default_timer as timer
 
 
 def part1(polymer: str) -> int:
     index = 0
-    polymer = list(polymer)
+    polymer = polymer
     while (True):
         if len(polymer) == index + 1:
             return len(polymer)
         char1 = polymer[index]
         char2 = polymer[index + 1]
         if char1.lower() == char2.lower() and char1 != char2:  # Samma bokstav fast stor och liten (Aa)
-            polymer.pop(index + 1)
-            polymer.pop(index)
+            polymer = polymer.replace(polymer[index] + polymer[index + 1], '')
             index = 0
         else:
             if index == len(polymer):
@@ -25,10 +25,17 @@ def part1_test():
     test(part1('dabAcCaCBAcCcaDA'), 10)  # dabCBAcaDA
 
 
-def part2(input) -> int:
+def part2(polymer: str) -> int:
+    polymer_count = []
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'x', 'y', 'z']
-    while(True):
+    for letter in letters:
+        polymer_copy = polymer
+        polymer_copy = polymer_copy.replace(letter, '')
+        polymer_copy = polymer_copy.replace(letter.upper(), '')
+        polymer_count.append(part1(polymer_copy))
+    polymer_count.sort()
+    return polymer_count[0]
 
 
 
@@ -39,7 +46,13 @@ def part2_test():
 print(tree)
 print('### Part 1 ###')
 part1_test()
+start = timer()
 print('Resultat: ' + str(part1(read_file_to_string('dec_05.txt'))))
+end = timer()
+print('Tid: {0:.3f}s'.format(end - start))
 print('### Part 2 ###')
 part2_test()
+start = timer()
 print('Resultat: ' + str(part2(read_file_to_string('dec_05.txt'))))
+end = timer()
+print('Tid: {0:.3f}s'.format(end - start))
--- template.py
+++ template.py
@@ -1,6 +1,7 @@
 from puzzle_util.christmas_tree import tree
 # from puzzle_util.read_file import någon_func
 from puzzle_util.test import test
+from timeit import default_timer as timer
 
 
 def part1(input) -> int:
@@ -22,7 +23,13 @@ def part2_test():
 print(tree)
 print('### Part 1 ###')
 part1_test()
-print('Resultat: ' + str(part1("")))
+start = timer()
+print('Resultat: ' + str(part1('')))
+end = timer()
+print('Tid: {0:.3f}s'.format(end - start))
 print('### Part 2 ###')
 part2_test()
-print('Resultat: ' + str(part2("")))
+start = timer()
+print('Resultat: ' + str(part2('')))
+end = timer()
+print('Tid: {0:.3f}s'.format(end - start))
